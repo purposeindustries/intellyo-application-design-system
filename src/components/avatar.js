01321d6b2c1e13d5c1f7634995a71fb70fdf4aeb
kristof0425 <dombi.kristof@gmail.com>
2017-08-01 12:25:26 +0200

    feat(avatar): add location and job title view

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UserAvatar from 'react-user-avatar';
 import classNames from 'classnames';
+import Icon from './icon';
 
 const Avatar = (props) => {
   return (
     <div
       className={ classNames('user-avatar', {
         'user-avatar--small': props.size === 'small',
-        'user-avatar--medium': props.size === 'medium'
+        'user-avatar--medium': props.size === 'medium',
+        'user-avatar--horizontal': props.location || props.jobPosition
       }) }
     >
       <UserAvatar
@@ -22,6 +24,21 @@ const Avatar = (props) => {
         src={ props.src }
         name={ props.name }
       />
+      { props.jobPosition && (
+        <div className="avatar-job-position-wrapper">
+          <span className="avatar-name">{ props.name }</span>
+          <span className="avatar-job-position">{ props.jobPosition }</span>
+        </div>
+      ) }
+      { props.location && (
+        <div className="avatar-location-wrapper">
+          <span className="avatar-name">{ props.name }</span>
+          <div className="location-wrap">
+            <Icon icon="ion-ios-location" />
+            <span className="avatar-location">{ props.location }</span>
+          </div>
+        </div>
+      ) }
     </div>
   );
 };
@@ -31,6 +48,8 @@ Avatar.displayName = 'Avatar';
 Avatar.propTypes = {
   size: PropTypes.string,
   name: PropTypes.string,
+  jobPosition: PropTypes.string,
+  location: PropTypes.string,
   src: PropTypes.src
 };
 

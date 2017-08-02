217111411805ce1e716ad48926b7c36d03eb5f6f
kristof0425 <dombi.kristof@gmail.com>
2017-08-02 16:39:01 +0200

    refactor(textarea): make it to stateless function 🔨

--- src/components/textarea.js
+++ src/components/textarea.js
@@ -1,51 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
-class Textarea extends Component {
-  static displayName = 'Textarea'
+const Textarea = (props) => (
+  <textarea
+    className="textarea"
+    onChange={ (e) => props.onChange(e) }
+    placeholder={ props.placeholder }
+  />
+);
 
-  static propTypes = {
-    charLimit: PropTypes.number,
-    placeholder: PropTypes.string
-  }
+Textarea.displayName = 'Textarea';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      charCount: this.props.charLimit
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+Textarea.defaultProps = {
+  onChange: () => {}
+};
 
-  handleChange(e) {
-    this.setState({
-      charCount: e.target.value.length === 0 ?
-        this.props.charLimit
-        : this.props.charLimit - e.target.value.length
-    });
-  }
-
-  render() {
-    return (
-      <div className="textarea">
-        <textarea
-          onChange={ this.handleChange }
-          placeholder={ this.props.placeholder }
-        />
-        { this.props.charLimit && (
-          <span
-            className={ classNames('char-counter', {
-              'over-limit': this.state.charCount < 0
-            }) }
-          >
-            { this.state.charCount }
-          </span>
-        ) }
-      </div>
-    );
-  }
-
-}
+Textarea.propTypes = {
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func
+};
 
 export default Textarea;

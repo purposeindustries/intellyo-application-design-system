b14215cdb00130a834476305674fff53df1a3a6d
kristof0425 <dombi.kristof@gmail.com>
2017-08-03 16:19:36 +0200

    feat(with-limit): change charLimit to a HOC

---
+++ src/components/with-limit.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+export default function withLimit(Component) {
+  function charLimit(props) {
+    return (
+      <div className="char-limit">
+        <Component
+          { ...props }
+          value={ props.value }
+          onChange={ props.onChange }
+        />
+        <span
+          className={ classNames('char-counter', {
+            'over-limit': props.limit - props.value.length < 0
+          }) }
+        >
+          { props.limit - props.value.length }
+        </span>
+      </div>
+    );
+  }
+
+  charLimit.propTypes = {
+    limit: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+
+  charLimit.displayName = `withLimit(${Component.displayName
+    || Component.name
+    || 'Component'
+  })`;
+
+  return charLimit;
+
+}

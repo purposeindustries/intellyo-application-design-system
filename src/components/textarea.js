18403f2e456e928d570331711f3ccdf47ed0a7d1
kristof0425 <dombi.kristof@gmail.com>
2017-08-03 11:51:45 +0200

    feat(disabled): add styles for disabled state

--- src/components/textarea.js
+++ src/components/textarea.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const Textarea = (props) => (
   <textarea
-    className="textarea"
+    className={ classNames('textarea', {
+      'textarea--disabled': props.disabled
+    }) }
     onChange={ props.onChange }
     placeholder={ props.placeholder }
     disabled={ props.disabled }

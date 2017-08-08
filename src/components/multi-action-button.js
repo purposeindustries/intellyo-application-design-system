8bb9650285524603ffca4b1dcf02d6dff0eddb09
Tamas Csonka <csnktms@gmail.com>
2017-08-08 16:20:38 +0200

    feat(): add props

---
+++ src/components/multi-action-button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const MultiActionButton = (props) => (
+  <button>
+
+  </button>
+);
+
+MultiActionButton.displayName = 'Multi Action Button';
+
+MultiActionButton.propTypes = {};
+
+export default MultiActionButton;

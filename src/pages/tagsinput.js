fe4002db7881fd0fb3e56e7a716611e58d51931d
kristof0425 <dombi.kristof@gmail.com>
2017-08-09 16:36:04 +0200

    feat(tagsinputs): add example of autosuggest

--- src/pages/tagsinput.js
+++ src/pages/tagsinput.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import TagsInput from '../components/tagsinput';
+import AutoSuggestTagsInput from '../components/autosuggest-tagsinput';
 import Card from '../components/card';
+
+const suggestions = [
+  {
+    photoSrc: 'http://placehold.it/20x20',
+    name: 'Foo'
+  },
+  {
+    photoSrc: 'http://placehold.it/20x20',
+    name: 'Bar'
+  },
+  {
+    photoSrc: 'http://placehold.it/20x20',
+    name: 'Baz'
+  }
+];
 
 export default class TagsInputPage extends Component {
 
@@ -10,12 +26,21 @@ export default class TagsInputPage extends Component {
     return (
       <div>
         <Card>
-          <TagsInput
-            onlyUnique
-            inputProps={ {
-              placeholder: 'Type your tag...'
-            } }
-          />
+          <div className="tagsinputs">
+            <TagsInput
+              onlyUnique
+              inputProps={ {
+                placeholder: 'Type your tag...'
+              } }
+            />
+            <AutoSuggestTagsInput
+              onlyUnique
+              suggestions={ suggestions }
+              inputProps={ {
+                placeholder: 'Search here for tags...'
+              } }
+            />
+          </div>
         </Card>
       </div>
     );

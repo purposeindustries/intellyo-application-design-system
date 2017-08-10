44bcef672a402f77b555c50a70a1bf25b6c279c8
kristof0425 <dombi.kristof@gmail.com>
2017-08-10 12:33:26 +0200

    feat(example): add richer examples

--- src/pages/tagsinput.js
+++ src/pages/tagsinput.js
@@ -5,16 +5,24 @@ import Card from '../components/card';
 
 const suggestions = [
   {
-    photoSrc: 'http://placehold.it/20x20',
-    name: 'Foo'
+    photoSrc: 'http://placehold.it/30x30',
+    name: 'Intel',
+    username: '@intelhungary'
   },
   {
-    photoSrc: 'http://placehold.it/20x20',
-    name: 'Bar'
+    photoSrc: 'http://placehold.it/30x30',
+    name: 'Intellyo',
+    username: '@intellyo'
   },
   {
-    photoSrc: 'http://placehold.it/20x20',
-    name: 'Baz'
+    photoSrc: 'http://placehold.it/30x30',
+    name: 'BMW',
+    username: '@bmwhungary'
+  },
+  {
+    photoSrc: 'http://placehold.it/30x30',
+    name: 'Renault Magyarorszag',
+    username: '@renaultmagyarorszag'
   }
 ];
 

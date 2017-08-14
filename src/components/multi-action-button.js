39b7f23310dae0d4c44db4a853f7db192178cff1
Tamas Csonka <csnktms@gmail.com>
2017-08-14 11:48:36 +0200

    feat(): remove comment

--- src/components/multi-action-button.js
+++ src/components/multi-action-button.js
@@ -7,9 +7,6 @@ const MultiActionButton = () => (
     <div className="multi-action-button-wrapper">
       <button type="button" className="multi-action-button multi-action-button-default">Default action</button>
       <DropdownButton />
-      {/* <button type="button" className="multi-action-button multi-action-button-default-caret">
-        <Icon icon="ion-android-arrow-dropdown" />
-      </button> */}
     </div>
     <ul className="multi-action-button-dropdown-menu">
       <li><a>{}</a></li>

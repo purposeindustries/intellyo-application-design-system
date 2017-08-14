84b2c57b1ac575076693bbd1bad929dc382db239
Tamas Csonka <csnktms@gmail.com>
2017-08-14 11:36:53 +0200

    feat(): add dropdown-menu

--- src/components/multi-action-button.js
+++ src/components/multi-action-button.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Icon from './icon';
 
 const MultiActionButton = () => (
-  <div className="multi-action-button-wrapper">
-    <button type="button" className="multi-action-button multi-action-button-default">Default action</button>
-    <button type="button" className="multi-action-button multi-action-button-default-caret"><Icon icon="ion-android-arrow-dropdown" /></button>
+  <div>
+    <div className="multi-action-button-wrapper">
+      <button type="button" className="multi-action-button multi-action-button-default">Default action</button>
+      <button type="button" className="multi-action-button multi-action-button-default-caret"><Icon icon="ion-android-arrow-dropdown" /></button>
+    </div>
+    <ul className="multi-action-button-dropdown-menu">
+      <li><a>Action one</a></li>
+      <li><a>Some other action</a></li>
+      <li><a>Action two</a></li>
+      <li><a>Action, action</a></li>
+    </ul>
   </div>
 );
 

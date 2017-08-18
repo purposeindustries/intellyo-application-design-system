e75d25b17f191a2585c76b835eafb7d521562b49
Tamas Csonka <csnktms@gmail.com>
2017-08-18 10:45:21 +0200

    feat(popover): add popover.js to src/components

---
+++ src/components/popover.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+const Popover = () => (
+  <div className="popover">
+  </div>
+);
+
+Popover.displayName = 'Popover';
+
+// Popover.propTypes = {
+//   children: PropTypes.string,
+//   placement: PropTypes.string,
+// };
+
+export default Popover;

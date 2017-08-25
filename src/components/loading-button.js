670e2970b18f8a14e3e54c178b7e31b9580492ff
Tamas Csonka <csnktms@gmail.com>
2017-08-25 13:52:24 +0200

    feat(loading-button): set up LoadingButton class

---
+++ src/components/loading-button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class LoadingButton extends React.component {
+  static displayName = 'Loading Button'
+  static propTypes = {
+
+  }
+  state = {}
+  checkRequest() {
+    return
+  }
+
+  render() {
+    return (
+      <button className="loading-button">
+
+      </button>
+    );
+  }
+}

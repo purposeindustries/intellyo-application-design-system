ea04dd8b9e9498ef2e2fe3b8e751a023d1c0eee6
Tamas Csonka <csnktms@gmail.com>
2017-08-28 15:57:29 +0200

    feat(loading-button): refactor

--- src/components/loading-button.js
+++ src/components/loading-button.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import LoadingIcon from './loading-icon';
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 export default class LoadingButton extends React.Component {
   static displayName = 'Loading Button'
   state = {
     loading: false,
   }
-  checkRequest = () => (
-    async () => {
-      this.setState({
-        loading: !this.state.loading
-      });
-      await sleep(3000);
-      this.setState({
-        loading: !this.state.loading
-      });
-      function sleep(ms) {
-        return new Promise(resolve => setTimeout(
-          resolve, ms
-        ));
-      }
-    }
-  )
+  checkRequest = async () => {
+    this.setState({
+      loading: !this.state.loading
+    });
+    await sleep(3000);
+    this.setState({
+      loading: !this.state.loading
+    });
+  }
   render() {
     return (
       <button

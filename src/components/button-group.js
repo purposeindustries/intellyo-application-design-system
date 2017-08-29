0b6bac917f21496b774c0d128cc8a71a95af2b4f
Tamas Csonka <csnktms@gmail.com>
2017-08-29 13:20:41 +0200

    feat(button-group): add basic button-group component

---
+++ src/components/button-group.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ButtonGroup = (props) => {
+  return (
+    <div className="button-group">
+      { props.children }
+    </div>
+  );
+};
+
+ButtonGroup.displayName = 'Button Group';
+
+ButtonGroup.propTypes = {
+  children: PropTypes.node,
+};

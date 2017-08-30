86b683e948f0c31411f83b64da1292984e39204c
Bence Dányi <bence@danyi.me>
2017-08-30 11:40:39 +0200

    feat: add expander icon

--- src/pages/tables.js
+++ src/pages/tables.js
@@ -3,12 +3,14 @@ import DisplayText from '../components/display-text';
 import Card from '../components/card';
 import Row from '../components/row';
 import Col from '../components/col';
+import Icon from '../components/icon';
 import {Table, Column, Expander} from '../components/table';
 import lipsum from 'lorem-ipsum';
+import c from 'classnames';
 
 const data = [];
-
-for (let i = 0; i < 20; i++) {
+let i;
+for (i = 0; i < 30; i++) {
   data.push({
     id: i + 1,
     title: lipsum({count: 1, units: 'sentences'}),
@@ -17,13 +19,39 @@ for (let i = 0; i < 20; i++) {
 }
 
 const toggleExpander = (prop, item, column, instance) => (
-  <button onClick={() => instance.toggleExpand()}>v</button>
+  <Icon icon='ion-arrow-right-b' className={c('table--expander-button', {
+    'table--expander-button--expanded': instance.state.expanded,
+  })} onClick={() => instance.toggleExpand()} />
 );
 
 const renderExpander = item => item.description;
 
 export default class Tables extends React.Component {
-  displayName = 'TablesPage'
+  static displayName = 'TablesPage'
+
+  state = {
+    data: data.slice(),
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      if (Math.random() > 0.5) {
+        const index = Math.floor(Math.random() * this.state.data.length);
+        this.setState({
+          data: this.state.data.filter((_, i) => i != index),
+        });
+      } else {
+        this.setState({
+          data: [...this.state.data, {
+            id: ++i,
+            title: lipsum({count: 1, units: 'sentences'}),
+            description: lipsum({count: 2, units: 'paragraphs'}),
+          }],
+        });
+      }
+    }, 1000);
+  }
+
   render() {
     return (
       <div>
@@ -32,7 +60,7 @@ export default class Tables extends React.Component {
           <Card>
             <Row>
               <Col span={ 12 }>
-                <Table data={data} sticky>
+                <Table data={data.slice()} sticky>
                   <Column name='expander' label='' renderCell={toggleExpander} />
                   <Column name='id' label='ID'/>
                   <Column name='title' label='Title' />
@@ -40,7 +68,15 @@ export default class Tables extends React.Component {
                 </Table>
               </Col>
               <Col span={ 12 }>
-                <Table data={data}>
+                <Table data={data.slice(0, 10)}>
+                  <Column name='expander' label='' renderCell={toggleExpander} />
+                  <Column name='id' label='ID'/>
+                  <Column name='title' label='Title' />
+                  <Expander render={renderExpander} />
+                </Table>
+              </Col>
+              <Col span={ 12 }>
+                <Table data={this.state.data}>
                   <Column name='expander' label='' renderCell={toggleExpander} />
                   <Column name='id' label='ID'/>
                   <Column name='title' label='Title' />

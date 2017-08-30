cd1bfc5db13cfeb836ce6b0e05d28413f4110385
Tamas Csonka <csnktms@gmail.com>
2017-08-30 12:29:35 +0200

    feat(heading): add folder, add index.js fix routing

--- src/pages/typography.js
+++ src/pages/typography.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DisplayText from '../components/display-text/';
-import Heading from '../components/heading';
+import Heading from '../components/heading/';
 import Subheading from '../components/subheading';
 import Caption from '../components/caption/';
 import Row from '../components/row';

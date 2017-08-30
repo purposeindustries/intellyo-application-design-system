0d8ed4e2e43f8741aa4f61b86044bf703347f53b
Tamas Csonka <csnktms@gmail.com>
2017-08-30 13:26:54 +0200

    feat(textarea): add folder, add index.js fix routing

--- src/pages/inputs.js
+++ src/pages/inputs.js
@@ -5,7 +5,7 @@ import Select from '../components/select/';
 import DropdownItem from '../components/dropdown-item/';
 import PrefixedInput, { PrefixedItem } from '../components/prefixed-input/';
 import SocialPrefixedInput from '../components/social-prefixed-input/';
-import Textarea from '../components/textarea';
+import Textarea from '../components/textarea/';
 import withLimit from '../components/with-limit';
 import Card from '../components/card/';
 import Row from '../components/row/';

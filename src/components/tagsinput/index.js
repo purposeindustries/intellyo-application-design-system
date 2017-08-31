51dab605a42dc44b4e71668e3cd8d9028e1607fb
kristof0425 <dombi.kristof@gmail.com>
2017-08-31 11:14:54 +0200

    feat(responsive-tagsinput): add new component

--- src/components/tagsinput/index.js
+++ src/components/tagsinput/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactTagsInput from 'react-tagsinput';
+import HideAt from 'react-hide-at';
+import ShowAt from 'react-show-at';
 import Button from '../button';
 import Input from '../input';
 import Icon from '../icon';
@@ -225,3 +227,36 @@ class TagsInput extends Component {
 }
 
 export default TagsInput;
+
+export const ResponsiveTagsInput = (props) => (
+  <div className="responsive-tagsinput">
+    <HideAt
+      breakpoint="small"
+      breakpoints={ props.breakpoints }
+    >
+      <TagsInput
+        { ...props }
+        renderInput={ props.renderDefaultInput }
+        size="normal"
+      />
+    </HideAt>
+    <ShowAt
+      breakpoint="small"
+      breakpoints={ props.breakpoints }
+    >
+      <TagsInput
+        { ...props }
+        renderInput={ props.renderStandAloneInput }
+      />
+    </ShowAt>
+  </div>
+);
+
+ResponsiveTagsInput.propTypes = {
+  breakpoints: PropTypes.object,
+  renderStandAloneInput: PropTypes.func,
+  renderDefaultInput: PropTypes.func,
+  size: PropTypes.string
+};
+
+ResponsiveTagsInput.displayName = 'ResponsiveTagsInput';

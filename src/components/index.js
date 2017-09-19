e1bd4994c5bce94bddf94a014e13c8d32fa5aae0
kristof0425 <dombi.kristof@gmail.com>
2017-09-19 10:47:14 +0200

    fix(export): remove default as

--- src/components/index.js
+++ src/components/index.js
@@ -36,5 +36,5 @@ export { default as Tooltip } from './tooltip';
 export { default as withLimit } from './with-limit';
 export { Table, Column, Expander } from './table';
 export { default as HorizontallyScrollableRow } from './horizontally-scrollable-row';
-export { default as Accordion, AccordionItem, SingleAccordion, MultiAccordion } from './accordion';
+export { Accordion, AccordionItem, SingleAccordion, MultiAccordion } from './accordion';
 export { default as EmptyView } from './empty-view';

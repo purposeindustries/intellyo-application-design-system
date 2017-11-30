a0d5a5805d3d71f5e2709ed28f2d7589e656f119
Kristof Molnosi <kristofmolnosi@Kristofs-MacBook-Pro.local>
2017-11-30 15:37:44 +0100

    feat: add unsaved changes

--- E2E/test/trial-test.js
+++ E2E/test/trial-test.js
@@ -1,4 +1,4 @@
-const ASSERT = require('assert');
+const assert = require('assert');
 const COMPONENTS_MENU = '=Components';
 const BUTTONS_MENU = '=Buttons';
 const SAVE_BUTTON = 'span=Save';
@@ -8,7 +8,7 @@ describe('FEF basic Tests', function () {
 
   it('should open  with proper title', function () {
     browser.url('/');
-    ASSERT.equal(browser.getTitle(), 'Intellyo Application Design System');
+    assert.equal(browser.getTitle(), 'Intellyo Application Design System');
   });
 
   it('should check the loading button', function () {
@@ -16,18 +16,18 @@ describe('FEF basic Tests', function () {
 
     let title = browser.getTitle();
 
-    ASSERT(browser.isExisting(COMPONENTS_MENU));
+    assert(browser.isExisting(COMPONENTS_MENU));
     browser.click(COMPONENTS_MENU);
 
-    ASSERT(browser.isExisting(BUTTONS_MENU));
+    assert(browser.isExisting(BUTTONS_MENU));
     browser.click(BUTTONS_MENU);
 
     $(SAVE_BUTTON).scroll();
 
-    ASSERT(browser.isExisting(SAVE_BUTTON));
+    assert(browser.isExisting(SAVE_BUTTON));
     browser.click(SAVE_BUTTON);
 
-    ASSERT(browser.isExisting(LOADING_BUTTON));
+    assert(browser.isExisting(LOADING_BUTTON));
     browser.click(LOADING_BUTTON);
 
     $(SAVE_BUTTON).waitForExist(2100);

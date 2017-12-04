36fd258084e2ab0d9fbe50acde7090178206698b
Kristof Molnosi <kristofmolnosi@Kristofs-MacBook-Pro.local>
2017-12-04 13:08:15 +0100

    S1

--- wdio.conf.js
+++ wdio.conf.js
@@ -125,8 +125,8 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [driver],
-    user: process.env.SAUCE_LABS_USERNAME,
-    key: process.env.SAUCE_LABS_ACCES_KEY,
+    user: sauceLabsUsername,
+    key: saucelabsAccesKey,
     sauceConnect: true,
     //
     // Framework you want to run your specs with.

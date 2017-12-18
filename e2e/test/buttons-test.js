d2a467cee788ddfe1a0a3681e96dface087bd802
Kristof Molnosi <kristofmolnosi@Kristofs-MacBook-Pro.local>
2017-12-18 14:06:29 +0100

    feat: circle ci warning 1.1

--- e2e/test/buttons-test.js
+++ e2e/test/buttons-test.js
@@ -2,14 +2,16 @@ const assert = require('assert');
 const visualRegression = require('../utils/visual-regression');
 const { takeScreenshotAndGetWholePageCompareResult, takeScreenShotOfElement } = visualRegression;
 
+//Selectors/xpaths
 const SAVE_BUTTON_SELECTOR = 'span=Save';
 const LOADING_BUTTON = 'span=Loading...';
-const SAVE_BUTTON_NAME = 'Save-button';
-const SAVE_BUTTON_LOADING_NAME = 'Save-button-loading';
-
 const YEAH_DROPDOWN_XPATH = '//*[@class="dropdown"][1]//*[@class="dropdown-inner-wrap dropdown-inner-wrap--split"]';
 const SAVE_BUTTON_XPATH = '/html/body/div[1]/div/div/div/div/div[4]/div/div/div/button'; //todo: create a normal xpath
+
+//test names
 const YEAH_DROPDOWN_NAME = 'Yeah-button';
+const SAVE_BUTTON_NAME = 'Save-button';
+const SAVE_BUTTON_LOADING_NAME = 'Save-button-loading';
 
 describe('FEF buttons tests', () => {
 
@@ -17,7 +19,6 @@ describe('FEF buttons tests', () => {
     browser.url('/buttons');
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
     assert(takeScreenshotAndGetWholePageCompareResult(10));
-    assert(false);
   });
 
   it('should check the button: ' + YEAH_DROPDOWN_NAME, () => {
@@ -31,39 +32,25 @@ describe('FEF buttons tests', () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_XPATH).scroll();
     assert(browser.isExisting(SAVE_BUTTON_XPATH));
-    if (!process.env.CI || !process.env.TEST_PROVIDER === 'sauce') {
-      assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 99));
-    } else {
-      assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 22));
-    }
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 12));
 
     assert(browser.isExisting(SAVE_BUTTON_XPATH));
     browser.click(SAVE_BUTTON_XPATH);
 
     $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
-    if (!process.env.CI || !process.env.TEST_PROVIDER === 'sauce') {
-      assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 99));
-    } else {
-      if (!browser.desiredCapabilities.browserName.includes('firefox')) {
-        assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 22));
-      } else {
-        assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 10));
-      }
-    }
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 12));
   });
 
   it('should check the button: ' + SAVE_BUTTON_LOADING_NAME, () => {
-    if (!process.env.CI || !process.env.TEST_PROVIDER === 'sauce') {
-      browser.url('/buttons');
-      $(SAVE_BUTTON_SELECTOR).scroll();
-      assert(browser.isExisting(SAVE_BUTTON_XPATH));
+    browser.url('/buttons');
+    $(SAVE_BUTTON_SELECTOR).scroll();
+    assert(browser.isExisting(SAVE_BUTTON_XPATH));
 
-      assert(browser.isExisting(SAVE_BUTTON_SELECTOR));
-      browser.click(SAVE_BUTTON_SELECTOR);
+    assert(browser.isExisting(SAVE_BUTTON_SELECTOR));
+    browser.click(SAVE_BUTTON_SELECTOR);
 
-      assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 4));
-      $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
-    }
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 4));
+    $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
   });
 
   it('should check the loading button', () => {

9e323203613090b7034704b7c8a851f8a15efae0
Kristof Molnosi <kristofmolnosi@Kristofs-MacBook-Pro.local>
2018-01-03 17:59:18 +0100

    Add description to asserts fix

--- e2e/test/buttons-test.js
+++ e2e/test/buttons-test.js
@@ -18,38 +18,37 @@ describe('FEF buttons tests', () => {
   it('Checks the the fef buttons page title and browser compare visual regression', () => {
     browser.url('/buttons');
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
-    assert(takeScreenshotAndGetWholePageCompareResult(10), 'Whole page screenshot assert');
+    assert(takeScreenshotAndGetWholePageCompareResult(10), 'Whole FEF buttons page screenshot compare to a reference picture');
   });
 
   it('should check the button: ' + YEAH_DROPDOWN_NAME, () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_SELECTOR).scroll();
-    assert(browser.isExisting(YEAH_DROPDOWN_XPATH), 'Assert if ', YEAH_DROPDOWN_NAME, ' exists');
-    assert(takeScreenShotOfElement(YEAH_DROPDOWN_XPATH, 6), 'Assert screenshot of: ', YEAH_DROPDOWN_NAME);
+    assert(browser.isExisting(YEAH_DROPDOWN_XPATH), 'Check if "Yeah dropdown" existing in the DOM');
+    assert(takeScreenShotOfElement(YEAH_DROPDOWN_XPATH, 6), 'Check if "Yeah dropdown" is similar to the reference');
   });
 
   it('should check the button: ' + SAVE_BUTTON_NAME, () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_XPATH).scroll();
-    assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Assert if ', SAVE_BUTTON_NAME, ' exists');
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true), 'Assert screenshot of: ', SAVE_BUTTON_NAME);
+    assert(browser.isExisting(SAVE_BUTTON_XPATH), '1st check if save button existing in the DOM');
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true), ' 1st check if save button is similar to the reference');
 
-    assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Assert if ', SAVE_BUTTON_NAME, ' exists');
+    assert(browser.isExisting(SAVE_BUTTON_XPATH), '2nd check if save button existing in the DOM');
     browser.click(SAVE_BUTTON_XPATH);
 
     $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true), 'Assert screenshot of: ', SAVE_BUTTON_NAME);
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true) + '2nd check if save button is similar to the reference');
   });
 
-  it('should check the button: ' + SAVE_BUTTON_LOADING_NAME, () => {
+  it.only('should check the button: ' + SAVE_BUTTON_LOADING_NAME, () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_SELECTOR).scroll();
-    assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Assert if ', SAVE_BUTTON_LOADING_NAME, ' exists');
 
-    assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Assert if ', SAVE_BUTTON_LOADING_NAME, ' exists');
+    assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Check if save button existing in the DOM for Loading button test');
     browser.click(SAVE_BUTTON_SELECTOR);
 
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 4), 'Assert screenshot of: ', SAVE_BUTTON_LOADING_NAME);
+    assert(takeScreenShotOfElement(LOADING_BUTTON, 4), 'Check if loading button is similar to the reference');
     $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
   });
 
@@ -58,10 +57,10 @@ describe('FEF buttons tests', () => {
 
     $(SAVE_BUTTON_SELECTOR).scroll();
 
-    assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Assert if ', SAVE_BUTTON_SELECTOR, ' exists');
+    assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Check if save button existing in the DOM for Loading button test');
     browser.click(SAVE_BUTTON_SELECTOR);
 
-    assert(browser.isExisting(LOADING_BUTTON), 'Assert if ', LOADING_BUTTON, ' exists');
+    assert(browser.isExisting(LOADING_BUTTON), 'Check if loading button appears in the DOM after click save button');
     browser.click(LOADING_BUTTON);
 
     $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
--- e2e/test/tags-input-test.js
+++ e2e/test/tags-input-test.js
@@ -20,45 +20,45 @@ describe('FEF tags input tests', () => {
   it('Checks the the fef buttons page title and browser compare visual regression', () => {
     browser.url('/tagsinput');
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
-    assert(takeScreenshotAndGetWholePageCompareResult(5), 'Whole page screenshot assert');
+    assert(takeScreenshotAndGetWholePageCompareResult(5), 'Whole FEF tags input page screenshot compare to a reference picture');
   });
 
   it('should check the input: ' + TEST_NAME_TAGS_INPUT_NAME, () => {
     browser.url('/tagsinput');
-    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ', TEST_NAME_TAGS_INPUT_NAME, ' exists');
-    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_ORIG_XPATH, 10), 'Assert screenshot of: ', TEST_NAME_TAGS_INPUT_NAME);
+    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Check if tags input existing in the DOM');
+    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_ORIG_XPATH, 10), 'Check if "Yeah dropdown" is similar to the reference');
   });
 
   it('should check the input: ' + TEST_NAME_TAGS_INPUT_MOUSE_OVER, () => {
     if (!browser.desiredCapabilities.browserName.includes('firefox')) { //TODO: We should find solutions for moveToObject and other cross-browser problems
       browser.url('/tagsinput');
-      assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ', TEST_NAME_TAGS_INPUT_MOUSE_OVER, ' exists');
+      assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ' + TEST_NAME_TAGS_INPUT_MOUSE_OVER + ' exists');
       browser.moveToObject(TAGS_INPUT_CLICKABLE, 0, 0);
-      assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_ORIG_XPATH, 10), 'Assert screenshot of: ', TEST_NAME_TAGS_INPUT_MOUSE_OVER);
+      assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_ORIG_XPATH, 10), 'Assert screenshot of: ' + TEST_NAME_TAGS_INPUT_MOUSE_OVER);
     }
   });
 
   it('should check the input: ' + TEST_NAME_TAGS_INPUT_CLICK, () => {
     browser.url('/tagsinput');
-    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ', TEST_NAME_TAGS_INPUT_CLICK, ' exists');
+    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ' + TEST_NAME_TAGS_INPUT_CLICK + ' exists');
     browser.click(TAGS_INPUT_CLICKABLE);
-    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_CLICKED_XPATH, 10), 'Assert screenshot of: ', TEST_NAME_TAGS_INPUT_CLICK);
+    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_CLICKED_XPATH, 10), 'Assert screenshot of: ' + TEST_NAME_TAGS_INPUT_CLICK);
   });
 
   it('should check the input: ' + TEST_NAME_TAGS_ADD_INPUT, () => {
     browser.url('/tagsinput');
-    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ', TEST_NAME_TAGS_ADD_INPUT, ' exists');
+    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ' + TEST_NAME_TAGS_ADD_INPUT + ' exists');
     browser.click(TAGS_INPUT_CLICKABLE);
     browser.setValue(TAGS_INPUT_INPUT, `${'foobar'}\n`);
-    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_CLICKED_XPATH, 11), 'Assert screenshot of: ', TEST_NAME_TAGS_ADD_INPUT);
+    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_CLICKED_XPATH, 11), 'Assert screenshot of: ' + TEST_NAME_TAGS_ADD_INPUT);
   });
 
   it('should check the input: ' + TEST_NAME_TAGS_ADD_INPUT_CLICK, () => {
     browser.url('/tagsinput');
-    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ', TEST_NAME_TAGS_ADD_INPUT_CLICK, ' exists');
+    assert(browser.isExisting(TAGS_INPUT_WHOLE_ORIG_XPATH), 'Assert if ' + TEST_NAME_TAGS_ADD_INPUT_CLICK + ' exists');
     browser.click(TAGS_INPUT_CLICKABLE);
     browser.setValue(TAGS_INPUT_INPUT, `${'foobar'}\n`);
     browser.click('span=hamburger');
-    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_ORIG_XPATH, 11), 'Assert screenshot of: ', TEST_NAME_TAGS_ADD_INPUT_CLICK);
+    assert(takeScreenShotOfElement(TAGS_INPUT_WHOLE_ORIG_XPATH, 11), 'Assert screenshot of: ' + TEST_NAME_TAGS_ADD_INPUT_CLICK);
   });
 });

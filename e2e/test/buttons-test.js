30f5a57e00d986b0db47c72dd4a044b7a44b758d
Kristof Molnosi <kristofmolnosi@Kristofs-MacBook-Pro.local>
2018-01-03 15:53:44 +0100

    Add description to asserts

--- e2e/test/buttons-test.js
+++ e2e/test/buttons-test.js
@@ -18,38 +18,38 @@ describe('FEF buttons tests', () => {
   it('Checks the the fef buttons page title and browser compare visual regression', () => {
     browser.url('/buttons');
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
-    assert(takeScreenshotAndGetWholePageCompareResult(10));
+    assert(takeScreenshotAndGetWholePageCompareResult(10), 'Whole page screenshot assert');
   });
 
   it('should check the button: ' + YEAH_DROPDOWN_NAME, () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_SELECTOR).scroll();
-    assert(browser.isExisting(YEAH_DROPDOWN_XPATH));
-    assert(takeScreenShotOfElement(YEAH_DROPDOWN_XPATH, 6));
+    assert(browser.isExisting(YEAH_DROPDOWN_XPATH), 'Assert if ', YEAH_DROPDOWN_NAME, ' exists');
+    assert(takeScreenShotOfElement(YEAH_DROPDOWN_XPATH, 6), 'Assert screenshot of: ', YEAH_DROPDOWN_NAME);
   });
 
   it('should check the button: ' + SAVE_BUTTON_NAME, () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_XPATH).scroll();
-    assert(browser.isExisting(SAVE_BUTTON_XPATH));
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true));
+    assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Assert if ', SAVE_BUTTON_NAME, ' exists');
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true), 'Assert screenshot of: ', SAVE_BUTTON_NAME);
 
-    assert(browser.isExisting(SAVE_BUTTON_XPATH));
+    assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Assert if ', SAVE_BUTTON_NAME, ' exists');
     browser.click(SAVE_BUTTON_XPATH);
 
     $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true));
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true), 'Assert screenshot of: ', SAVE_BUTTON_NAME);
   });
 
   it('should check the button: ' + SAVE_BUTTON_LOADING_NAME, () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_SELECTOR).scroll();
-    assert(browser.isExisting(SAVE_BUTTON_XPATH));
+    assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Assert if ', SAVE_BUTTON_LOADING_NAME, ' exists');
 
-    assert(browser.isExisting(SAVE_BUTTON_SELECTOR));
+    assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Assert if ', SAVE_BUTTON_LOADING_NAME, ' exists');
     browser.click(SAVE_BUTTON_SELECTOR);
 
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 4));
+    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 4), 'Assert screenshot of: ', SAVE_BUTTON_LOADING_NAME);
     $(SAVE_BUTTON_SELECTOR).waitForExist(2200);
   });
 
@@ -58,10 +58,10 @@ describe('FEF buttons tests', () => {
 
     $(SAVE_BUTTON_SELECTOR).scroll();
 
-    assert(browser.isExisting(SAVE_BUTTON_SELECTOR));
+    assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Assert if ', SAVE_BUTTON_SELECTOR, ' exists');
     browser.click(SAVE_BUTTON_SELECTOR);
 
-    assert(browser.isExisting(LOADING_BUTTON));
+    assert(browser.isExisting(LOADING_BUTTON), 'Assert if ', LOADING_BUTTON, ' exists');
     browser.click(LOADING_BUTTON);
 
     $(SAVE_BUTTON_SELECTOR).waitForExist(2200);

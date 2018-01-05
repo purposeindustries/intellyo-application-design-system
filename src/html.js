c09f309b708a887167fd19bea44fd0801ed848fa
kristof0425 <dombi.kristof@gmail.com>
2018-01-05 16:42:03 +0100

    chore(html.js): use html.js to be able to use react-portal

---
+++ src/html.js
@@ -0,0 +1,57 @@
+/* eslint
+  react/prop-types: "off",
+  no-console: "off",
+  react/display-name: "off",
+  react/no-danger: "off",
+  no-process-env: "off"
+*/
+
+import React from 'react';
+
+let stylesStr;
+if (process.env.NODE_ENV === 'production') {
+  try {
+    stylesStr = require('!raw-loader!../public/styles.css');
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = class HTML extends React.Component {
+  render() {
+    let css;
+    if (process.env.NODE_ENV === 'production') {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          eslint-disable-next-line
+          dangerouslySetInnerHTML={ { __html: stylesStr } }
+        />
+      );
+    }
+    return (
+      <html { ...this.props.htmlAttributes }>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          { this.props.headComponents }
+          {css}
+        </head>
+        <body { ...this.props.bodyAttributes }>
+          { this.props.preBodyComponents }
+          <div
+            key={ 'body' }
+            id="___gatsby"
+            dangerouslySetInnerHTML={ { __html: this.props.body } }
+          />
+          <div id="floating-container" />
+          { this.props.postBodyComponents }
+        </body>
+      </html>
+    );
+  }
+};

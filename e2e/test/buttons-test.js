6770a4d8658628fd854ec79003516f984cbfbdce
Kristof Molnosi <kristofmolnosi@Kristofs-MacBook-Pro.local>
2018-01-05 13:42:56 +0100

    try

--- e2e/test/buttons-test.js
+++ e2e/test/buttons-test.js
@@ -1,50 +1,50 @@
-const assert = require('assert');
-const visualRegression = require('../utils/visual-regression');
-const { takeScreenshotAndGetWholePageCompareResult, takeScreenShotOfElement } = visualRegression;
-
-//Selectors/xpaths
-const SAVE_BUTTON_SELECTOR = 'span=Save';
-const YEAH_DROPDOWN_XPATH = '//*[@class="dropdown"][1]//*[@class="dropdown-inner-wrap dropdown-inner-wrap--split"]';
-const SAVE_BUTTON_XPATH = '/html/body/div[1]/div/div/div/div/div[4]/div/div/div/button'; //TODO: create a normal xpath
-
-//test names
-const YEAH_DROPDOWN_NAME = 'Yeah-button';
-const SAVE_BUTTON_NAME = 'Save-button';
-const SAVE_BUTTON_LOADING_NAME = 'Save-button-loading';
-
-describe('FEF buttons tests', () => {
-
-  it.skip('Checks the the fef buttons page title and browser compare visual regression', () => {
-    browser.url('/buttons');
-    assert.equal(browser.getTitle(), 'Intellyo Application Design System');
-    assert(takeScreenshotAndGetWholePageCompareResult(10), 'Whole FEF buttons page screenshot compare to a reference picture');
-  });
-
-  it('should check the button: ' + YEAH_DROPDOWN_NAME, () => {
-    browser.url('/buttons');
-    $(SAVE_BUTTON_SELECTOR).scroll();
-    assert(browser.isExisting(YEAH_DROPDOWN_XPATH), '"Yeah dropdown" is not existing in the DOM');
-    assert(takeScreenShotOfElement(YEAH_DROPDOWN_XPATH, 6), '"Yeah dropdown" is not similar to the reference');
-  });
-
-  it('should check the button: ' + SAVE_BUTTON_NAME, () => {
-    browser.url('/buttons');
-    $(SAVE_BUTTON_XPATH).scroll();
-    assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Save button is not existing in the DOM');
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true), 'Save button is not similar to the reference before click');
-    browser.click(SAVE_BUTTON_XPATH);
-
-    $(SAVE_BUTTON_SELECTOR).waitForExist(8000);
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true) + '2nd check if save button is similar to the reference');
-  });
-
-  it('should check the button: ' + SAVE_BUTTON_LOADING_NAME, () => {
-    browser.url('/buttons');
-    $(SAVE_BUTTON_SELECTOR).scroll();
-    assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Save button is not existing in the DOM before click for loading button');
-    browser.click(SAVE_BUTTON_SELECTOR);
-
-    assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 4), 'Loading button is not similar to the reference after click loading');
-    $(SAVE_BUTTON_SELECTOR).waitForExist(8000);
-  });
-});
+// const assert = require('assert');
+// const visualRegression = require('../utils/visual-regression');
+// const { takeScreenshotAndGetWholePageCompareResult, takeScreenShotOfElement } = visualRegression;
+//
+// //Selectors/xpaths
+// const SAVE_BUTTON_SELECTOR = 'span=Save';
+// const YEAH_DROPDOWN_XPATH = '//*[@class="dropdown"][1]//*[@class="dropdown-inner-wrap dropdown-inner-wrap--split"]';
+// const SAVE_BUTTON_XPATH = '/html/body/div[1]/div/div/div/div/div[4]/div/div/div/button'; //TODO: create a normal xpath
+//
+// //test names
+// const YEAH_DROPDOWN_NAME = 'Yeah-button';
+// const SAVE_BUTTON_NAME = 'Save-button';
+// const SAVE_BUTTON_LOADING_NAME = 'Save-button-loading';
+//
+// describe('FEF buttons tests', () => {
+//
+//   it('Checks the the fef buttons page title and browser compare visual regression', () => {
+//     browser.url('/buttons');
+//     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
+//     assert(takeScreenshotAndGetWholePageCompareResult(10), 'Whole FEF buttons page screenshot compare to a reference picture');
+//   });
+//
+//   it('should check the button: ' + YEAH_DROPDOWN_NAME, () => {
+//     browser.url('/buttons');
+//     $(SAVE_BUTTON_SELECTOR).scroll();
+//     assert(browser.isExisting(YEAH_DROPDOWN_XPATH), '"Yeah dropdown" is not existing in the DOM');
+//     assert(takeScreenShotOfElement(YEAH_DROPDOWN_XPATH, 6), '"Yeah dropdown" is not similar to the reference');
+//   });
+//
+//   it('should check the button: ' + SAVE_BUTTON_NAME, () => {
+//     browser.url('/buttons');
+//     $(SAVE_BUTTON_XPATH).scroll();
+//     assert(browser.isExisting(SAVE_BUTTON_XPATH), 'Save button is not existing in the DOM');
+//     assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true), 'Save button is not similar to the reference before click');
+//     browser.click(SAVE_BUTTON_XPATH);
+//
+//     $(SAVE_BUTTON_SELECTOR).waitForExist(8000);
+//     assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 17, true) + '2nd check if save button is similar to the reference');
+//   });
+//
+//   it('should check the button: ' + SAVE_BUTTON_LOADING_NAME, () => {
+//     browser.url('/buttons');
+//     $(SAVE_BUTTON_SELECTOR).scroll();
+//     assert(browser.isExisting(SAVE_BUTTON_SELECTOR), 'Save button is not existing in the DOM before click for loading button');
+//     browser.click(SAVE_BUTTON_SELECTOR);
+//
+//     assert(takeScreenShotOfElement(SAVE_BUTTON_XPATH, 1), 'Loading button is not similar to the reference after click loading');
+//     $(SAVE_BUTTON_SELECTOR).waitForExist(8000);
+//   });
+// });
--- wdio.conf.js
+++ wdio.conf.js
@@ -2,17 +2,29 @@ require('dotenv').config();
 const slugify = require('slugify');
 const path = require('path');
 const VisualRegressionCompare = require('wdio-visual-regression-service/compare');
-const hostname = require('os').hostname();
+const os = require('os');
 const dateFormat = require('dateformat');
 
 const resolution = { width: 1024, height: 768 };
 const screenResolution = resolution.width.toString() + 'x' + resolution.height.toString();
 const browserName = 'chrome';
+const platform = process.platform;
 let isDefaultBrowser = true;
 let sauceLabsUsername;
 let saucelabsAccesKey;
 let driver = 'selenium-standalone';
 let browsers = [];
+let name;
+
+if (process.env.CIRCLE_PROJECT_USERNAME) {
+  name = [
+    process.env.CIRCLE_PROJECT_USERNAME,
+    process.env.CIRCLE_PROJECT_REPONAME,
+    process.env.CIRCLE_BRANCH
+  ].join('/') + '#' + process.env.CIRCLE_BUILD_NUM;
+} else {
+  name = os.userInfo().username + '@' + os.hostname();
+}
 
 if (process.env.CI || process.env.TEST_PROVIDER === 'sauce') {
   isDefaultBrowser = false;
@@ -61,9 +73,9 @@ function getScreenshotName(basePath) {
     const browserWidth = browserViewport.width;
     const browserHeight = browserViewport.height;
     const parent = context.test.parent;
-    const time = dateFormat(new Date(), 'dddd-mmmm-dS-yyyy-h-MM-ss-TT');
+    const time = dateFormat(new Date(), 'MM-ss-TT-yyyy-mm-dd');
 
-    return path.join(basePath, `${slugify(parent.toLowerCase())}/${slugify(testName.toLowerCase())}/${type}_${browserName.toLowerCase()}_v${browserVersion}_${browserWidth}x${browserHeight}_${slugify(time.toLowerCase())}.png`);
+    return path.join(basePath, `${slugify(parent.toLowerCase())}/${slugify(testName.toLowerCase())}/${slugify(time.toLowerCase())}_${slugify(platform.toLowerCase())}_${type}_${browserName.toLowerCase()}_v${browserVersion}_${browserWidth}x${browserHeight}.png`);
   };
 }
 
@@ -96,37 +108,37 @@ exports.config = {
     version: 'latest',
     screenResolution: screenResolution,
     platform: 'macOS 10.13'
-  }, {
-    browserName: 'chrome',
-    'chromeOptions': {
-      'args': ['disable-infobars']
-    },
-    version: 'latest',
-    screenResolution: screenResolution,
-    platform: 'macOS 10.13'
-  }, {
-    browserName: 'chrome',
-    version: 'latest-1',
-    screenResolution: screenResolution,
-    platform: 'Windows 10'
+  // }, {
+  //   browserName: 'chrome',
+  //   'chromeOptions': {
+  //     'args': ['disable-infobars']
+  //   },
+  //   version: 'latest',
+  //   screenResolution: screenResolution,
+  //   platform: 'macOS 10.13'
+  // }, {
+  //   browserName: 'chrome',
+  //   version: 'latest-1',
+  //   screenResolution: screenResolution,
+  //   platform: 'Windows 10'
   }, {
     browserName: 'firefox',
     version: 'latest',
     screenResolution: screenResolution,
     platform: 'Windows 10'
-  }, {
-    browserName: 'chrome',
-    'chromeOptions': {
-      'args': ['disable-infobars']
-    },
-    version: 'latest',
-    screenResolution: screenResolution,
-    platform: 'Windows 10'
-  }, {
-    browserName: 'chrome',
-    version: 'latest-1',
-    screenResolution: screenResolution,
-    platform: 'macOS 10.13'
+  // }, {
+  //   browserName: 'chrome',
+  //   'chromeOptions': {
+  //     'args': ['disable-infobars']
+  //   },
+  //   version: 'latest',
+  //   screenResolution: screenResolution,
+  //   platform: 'Windows 10'
+  // }, {
+  //   browserName: 'chrome',
+  //   version: 'latest-1',
+  //   screenResolution: screenResolution,
+  //   platform: 'macOS 10.13'
   }] : browsers,
 
   sync: true,
@@ -153,7 +165,7 @@ exports.config = {
   sauceConnect: true,
 
   sauceConnectOpts: {
-    tunnelIdentifier: hostname
+    tunnelIdentifier: name
   },
 
   framework: 'mocha',

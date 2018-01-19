aa83a47eb67b3cc8f99cbb3f6ece231e16bc9e46
Kristof Molnosi <molnosikristof@gmail.com>
2018-01-19 18:07:46 +0100

    fix: decrease mismatch tolerance

--- e2e/test/tootltip-tests/tooltip-test.js
+++ e2e/test/tootltip-tests/tooltip-test.js
@@ -7,7 +7,7 @@ describe('FEF tooltip tests', () => {
   it('Checks the the fef tooltips page title and browser compare visual regression', () => {
     browser.url('/tooltip');
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
-    assert(takeScreenshotAndGetWholePageCompareResult({defaultTolerance: 14, ignoreComparison: false}),
+    assert(takeScreenshotAndGetWholePageCompareResult({defaultTolerance: 3, ignoreComparison: false}),
       'Whole FEF tooltips page screenshot not similar to other configurations');
   });
 

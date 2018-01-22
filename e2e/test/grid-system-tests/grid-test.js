521846049814a379a0724d68424deedb409a8c5c
Kristof Molnosi <molnosikristof@gmail.com>
2018-01-22 16:25:09 +0100

    fix: change grid diff tolerance

--- e2e/test/grid-system-tests/grid-test.js
+++ e2e/test/grid-system-tests/grid-test.js
@@ -7,7 +7,7 @@ describe('FEF grid tests', () => {
   it('Checks the the fef grid page title and browser compare visual regression', () => {
     browser.url('/grid');
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
-    assert(takeScreenshotAndGetWholePageCompareResult({defaultTolerance: 3, ignoreComparison: false}),
+    assert(takeScreenshotAndGetWholePageCompareResult({defaultTolerance: 6, ignoreComparison: false}),
     'Whole FEF grid page screenshot compare to a reference picture');
   });
 });

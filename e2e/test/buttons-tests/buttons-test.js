f39a7e7def7ab271d90b1e89d9998a8e716b86bc
Kristof Molnosi <molnosikristof@gmail.com>
2018-01-31 09:36:10 +0100

    fix: try master to run

--- e2e/test/buttons-tests/buttons-test.js
+++ e2e/test/buttons-tests/buttons-test.js
@@ -25,9 +25,9 @@ describe('FEF buttons tests', () => {
     browser.url('/buttons');
     $(SAVE_BUTTON_SELECTOR).scroll();
     assert(browser.isExisting(YEAH_DROPDOWN_XPATH), '"Yeah dropdown" is not existing in the DOM');
-    browser.moveToElement(YEAH_DROPDOWN_XPATH);
-    assert(takeScreenShotOfElement2('.dropdown', {defaultTolerance: 6}),
-     '"Yeah dropdown" is not similar to the reference');
+    assert(takeScreenShotOfElement(YEAH_DROPDOWN_XPATH,
+      {defaultTolerance: 6, ignoreComparison: false}),
+       '"Yeah dropdown" is not similar to the reference');
   });
 
   it('should check the button: ' + SAVE_BUTTON_NAME, () => {

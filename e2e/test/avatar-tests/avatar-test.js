8376a593528a00900d3e7e35a9491dd3356b8fde
Kristof Molnosi <molnosikristof@gmail.com>
2018-02-06 19:39:29 +0100

    fix: change tolerance from 3 to 3.5

--- e2e/test/avatar-tests/avatar-test.js
+++ e2e/test/avatar-tests/avatar-test.js
@@ -7,7 +7,7 @@ describe('FEF avatars tests', () => {
   it('Checks the the fef tooltips page title and browser compare visual regression', () => {
     browser.url('/avatars');
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
-    assert(takeScreenshotAndGetWholePageCompareResult({defaultTolerance: 3, ignoreComparison: false}),
+    assert(takeScreenshotAndGetWholePageCompareResult({defaultTolerance: 3.5, ignoreComparison: false}),
       'Whole FEF avatars page screenshot not similar to other configurations');
   });
 

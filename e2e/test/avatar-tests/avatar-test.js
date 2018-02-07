1c54f5fe46787d9ece19f703f5f9eb202b62bbba
Kristof Molnosi <molnosikristof@gmail.com>
2018-02-07 14:18:36 +0100

    test: add basic pic upload pic

--- e2e/test/avatar-tests/avatar-test.js
+++ e2e/test/avatar-tests/avatar-test.js
@@ -1,6 +1,16 @@
 const assert = require('assert');
 const visualRegression = require('../../utils/visual-regression');
-const { takeScreenshotAndGetWholePageCompareResult } = visualRegression;
+const { takeScreenshotAndGetWholePageCompareResult, takeScreenShotOfElement } = visualRegression;
+
+//Selectors/xpaths
+const INPUT_SELECTOR = 'input[type=file]';
+const FIRST_AVATAR_SELECTOR = '.card-body:nth-child(2) .user-avatar';
+
+//test names
+const UPLOAD_FILE = 'upload-file';
+
+//pic path
+const PIC_PATH = '/Users/kristofmolnosi/repo/intellyo-application-design-system/e2e/utils/pics/IMG_0090.jpg';
 
 describe('FEF avatars tests', () => {
 
@@ -9,6 +19,14 @@ describe('FEF avatars tests', () => {
     assert.equal(browser.getTitle(), 'Intellyo Application Design System');
     assert(takeScreenshotAndGetWholePageCompareResult({defaultTolerance: 3.5, ignoreComparison: false}),
       'Whole FEF avatars page screenshot not similar to other configurations');
+  });
+
+  it('should check avatar ' + UPLOAD_FILE, () => {
+    browser.url('/avatars');
+    assert(browser.isExisting(INPUT_SELECTOR), 'Input selector is not existing in the DOM');
+    browser.chooseFile(INPUT_SELECTOR, PIC_PATH);
+    assert(takeScreenShotOfElement(FIRST_AVATAR_SELECTOR, {defaultTolerance: 11, ignoreComparison: false}),
+      'Avatar input screenshot not similar to other configurations');
   });
 
 });

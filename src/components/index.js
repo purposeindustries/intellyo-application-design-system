753c701bb3260a723d6affd1a2e63c7fda345dc7
Kristóf Dombi <dombi.kristof@gmail.com>
2018-02-09 15:48:45 +0100

    feat/add-tabpanel (#279)
    * feat(TabPanelPage): add example of TabPanel and Tab
    * docs(TabPanel, Tab): add readme of the two components
    * docs(README): add documentation of TabPanel

--- src/components/index.js
+++ src/components/index.js
@@ -47,5 +47,6 @@ export { default as ToggleableTags } from './toggleable-tags';
 export { default as AvatarEditor } from './avatar-editor';
 export { default as Box } from './box';
 export { default as Section } from './section';
+export { default as Tab, TabPanel } from './tab-panel';
 export { default as List } from './list';
 export { default as DragSort } from './list/dragSort';

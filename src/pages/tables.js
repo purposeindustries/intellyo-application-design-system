c313f45b7f2961c7a47ae28e4d370a30bf2872f1
Kristof Molnosi <molnosikristof@gmail.com>
2018-02-11 18:58:22 +0100

    fix: fix content for not infinite tables

--- src/pages/tables.js
+++ src/pages/tables.js
@@ -5,21 +5,10 @@ import Row from '../components/row';
 import Col from '../components/col';
 import Icon from '../components/icon';
 import {Table, Column, Expander} from '../components/table';
-import lipsum from 'lorem-ipsum';
 import c from 'classnames';
 import InfiniteTable from '../components/table/infinite-loading-table';
 import FakeFetch from '../utils/fake-fetch';
-
-const data = [];
-let i;
-for (i = 0; i < 20; i++) {
-  data.push({
-    id: i + 1,
-    title: lipsum({count: 1, units: 'sentences'}),
-    description: lipsum({count: 2, units: 'paragraphs'}),
-    tags: Array(Math.floor(Math.random() * 5)).join(0).split(0).map(() => lipsum({count: 1, units: 'words'})),
-  });
-}
+import data from '../utils/dummy-text.js';
 
 // eslint-disable-next-line react/display-name
 const toggleExpander = (prop, item, column, instance) => (

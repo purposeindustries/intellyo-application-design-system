2f8de4b780ac2364ea38e97f5463eb5b26e916d8
kristof0425 <dombi.kristof@gmail.com>
2018-03-08 16:20:27 +0100

    feat(AvatarCard): add initial component

---
+++ src/components/avatar-card/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Avatar from '../avatar';
+
+const AvatarCard = (props) => (
+  <div className="user-avatar-card">
+    <Avatar
+      name={ props.name }
+      src={ props.src }
+      showTooltip={ false }
+    />
+    <div className="user-avatar-card-caption-wrapper">
+      <span className="user-avatar-card-name">{ props.name }</span>
+      { props.caption && <div className="card-caption-wrap">
+        { props.icon }
+        <span className="user-avatar-card-caption">{ props.caption }</span>
+      </div> }
+    </div>
+  </div>
+);
+
+AvatarCard.displayName = 'AvatarCard';
+AvatarCard.propTypes = {
+  name: PropTypes.string,
+  src: PropTypes.string,
+  caption: PropTypes.node,
+  icon: PropTypes.node,
+};
+
+export default AvatarCard;

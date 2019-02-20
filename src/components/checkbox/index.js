b16f530ade4daf3e513db8b52f8c3123df54d5fa
Róbert Oroszi <robert@oroszi.net>
2019-02-20 16:22:23 +0100

    feat: add disabled checkbox state (#408)

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Icon } from '../index';
 
-const Checkbox = ({ checked, onChange }) => (
+const Checkbox = ({ checked, onChange, disabled }) => (
   <div
     role="checkbox"
-    onClick={ onChange }
-    onKeyDown={ (e) => e.keyCode === 13 && onChange() }
+    onClick={ disabled !== true && onChange }
+    onKeyDown={ (e) => disabled !== true && e.keyCode === 13 && onChange() }
     aria-checked={ checked }
+    aria-disabled={ disabled }
     tabIndex={ 0 }
     className="checkbox"
   >
@@ -19,10 +19,12 @@ const Checkbox = ({ checked, onChange }) => (
 Checkbox.displayName = 'Checkbox';
 Checkbox.propTypes = {
   checked: PropTypes.bool,
+  disabled: PropTypes.bool,
   onChange: PropTypes.func,
 };
 Checkbox.defaultProps = {
-  onChange: () => {}
+  onChange: () => {},
+  disabled: false
 };
 
 export default Checkbox;
